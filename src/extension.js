96564784157e7c81080007f09ea9439284c33800
Daniel Sieradski <daniel@self.agency>
2020-05-08 00:15:58 -0400

    all good, 0.1.10

--- src/extension.js
+++ src/extension.js
@@ -54,14 +54,16 @@ const activate = context => {
         param && param.fsPath
           ? param.fsPath
           : (vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length
-              ? `${vscode.workspace.workspaceFolders[0]}/`
+              ? `${vscode.workspace.workspaceFolders[0].uri.fsPath}/`
               : '') +
             (
               await vscode.window.showInputBox({
                 prompt: 'Folder in which to place your new component',
                 placeHolder: 'components'
               })
-            ).replace(/\/$/, '')
+            )
+              .replace(/\/$/, '')
+              .replace(/^\//, '')
 
       generateVueComponent(componentName, folderPath)
     } else {
